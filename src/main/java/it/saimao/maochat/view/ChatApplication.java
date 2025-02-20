52c9dc550f017932368c8f42c58a79d10a3f7b95
Sai Mao (DELL) <saisainghminetun@gmail.com>
2025-02-20 13:34:07 +0800

    Add logo for the app. Resize the app width and height!

--- src/main/java/it/saimao/maochat/view/ChatApplication.java
+++ src/main/java/it/saimao/maochat/view/ChatApplication.java
@@ -3,6 +3,7 @@ package it.saimao.maochat.view;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,9 +14,10 @@ public class ChatApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChatApplication.class.getResource("/fxml/choice-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 240, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 480);
         stage.setTitle("Mao Chat");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(getClass().getResource("/images/icon.png").toExternalForm()));
         stage.show();
     }
 
